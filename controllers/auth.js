39dbd270c337ece66af4aadca10ba045382bdae0
Anderson Rivera <anderzonrivera480@gmail.com>
2023-01-12 11:31:47 -0500

    google sign in implement

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,8 @@
-const { response } = require("express");
+const { response, json } = require("express");
 const User = require("../models/user");
 const bcryptjs = require("bcryptjs");
 const { generateJWT } = require("../helpers/generateJWT");
+const { googleVerify } = require("../helpers/google-verify");
 
 const login = async (req, res = response) => {
   const { email, pass } = req.body;
@@ -46,6 +47,55 @@ const login = async (req, res = response) => {
   }
 };
 
+const googleSignIn = async (req, res = response) => {
+  const { id_token } = req.body;
+
+  try {
+    const { name, img, email } = await googleVerify(id_token);
+
+    let user = await User.findOne({ email });
+
+    // si no existe lo creamos
+    if (!user) {
+      const data = {
+        name,
+        email,
+        pass: "@",
+        img,
+        role: "user",
+        google: true,
+      };
+
+      user = new User(data);
+      await user.save();
+    }
+
+    // Si el user en DB no esta activo
+    if (!user.status) {
+      return res.status(401).json({
+        status: "Error action",
+        message: "User not active, communicate with admin",
+      });
+    }
+
+    // Generar el jwt
+    const token = await generateJWT(user.id);
+
+    res.status(201).json({
+      status: "ok",
+      user,
+      token,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({
+      status: "Error action",
+      message: "Token not verified",
+    });
+  }
+};
+
 module.exports = {
   login,
+  googleSignIn,
 };
